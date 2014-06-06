ta264/nzbget_svn

commit ddc9df6d536cbbda1b385c681697bc503512c154

improved fast par-renamer: it can now detect missing files (files listed in par2-files but not present on disk)
git-svn-id: svn://svn.code.sf.net/p/nzbget/code/trunk@1030 b1b436ca-2b18-4f33-ab5d-510789358d54

daemon/postprocess/ParCoordinator.cpp
@@ -666,7 +666,12 @@ void ParCoordinator::ParRenameCompleted()
 	PostInfo* pPostInfo = m_ParRenamer.GetPostInfo();
 	pPostInfo->GetNZBInfo()->SetRenameStatus(m_ParRenamer.GetStatus() == ParRenamer::psSuccess ? NZBInfo::rsSuccess : NZBInfo::rsFailure);
 
-	if (m_ParRenamer.HasSplittedFragments() && pPostInfo->GetNZBInfo()->GetParStatus() <= NZBInfo::psSkipped)
+	if (m_ParRenamer.HasMissedFiles() && pPostInfo->GetNZBInfo()->GetParStatus() <= NZBInfo::psSkipped)
+	{
+		PrintMessage(pPostInfo, Message::mkInfo, "%s requested par-check/repair to restore missing files", m_ParRenamer.GetInfoName());
+		pPostInfo->SetRequestParCheck(true);
+	}
+	else if (m_ParRenamer.HasSplittedFragments() && pPostInfo->GetNZBInfo()->GetParStatus() <= NZBInfo::psSkipped)
 	{
 		PrintMessage(pPostInfo, Message::mkInfo, "%s requested par-check/repair to join splitted fragments", m_ParRenamer.GetInfoName());
 		pPostInfo->SetRequestParCheck(true);

daemon/postprocess/ParRenamer.cpp
@@ -86,6 +86,7 @@ ParRenamer::ParRenamer()
 	m_iStageProgress = 0;
 	m_bCancelled = false;
 	m_bHasSplittedFragments = false;
+	m_iMissedCount = 0;
 }
 
 ParRenamer::~ParRenamer()
@@ -143,6 +144,7 @@ void ParRenamer::Run()
 	m_iFileCount = 0;
 	m_iCurFile = 0;
 	m_iRenamedCount = 0;
+	m_iMissedCount = 0;
 	m_bHasSplittedFragments = false;
 	m_eStatus = psFailed;
 
@@ -159,6 +161,7 @@ void ParRenamer::Run()
 		debug("Checking %s", szDestDir);
 		ClearHashList();
 		LoadParFiles(szDestDir);
+		m_iFoundCount = 0;
 
 		if (m_FileHashList.empty())
 		{
@@ -169,8 +172,10 @@ void ParRenamer::Run()
 		}
 
 		CheckFiles(szDestDir, false);
+
+		m_iMissedCount += m_FileHashList.size() - m_iFoundCount;
 	}
-	
+
 	if (m_bCancelled)
 	{
 		PrintMessage(Message::mkWarning, "Renaming cancelled for %s", m_szInfoName);
@@ -183,6 +188,11 @@ void ParRenamer::Run()
 	else
 	{
 		PrintMessage(Message::mkInfo, "No renamed files found for %s", m_szInfoName);
+	}
+
+	if (m_iMissedCount > 0)
+	{
+		PrintMessage(Message::mkWarning, "Detected %i missing or damaged file(s) for %s", m_iMissedCount, m_szInfoName);
 	}
 
 	Cleanup();
@@ -365,6 +375,7 @@ void ParRenamer::CheckRegularFile(const char* szDestDir, const char* szFilename)
 		if (!strcmp(pFileHash->GetHash(), hash16k.print().c_str()))
 		{
 			debug("Found correct filename: %s", pFileHash->GetFilename());
+			m_iFoundCount++;
 
 			char szDstFilename[1024];
 			snprintf(szDstFilename, 1024, "%s%c%s", szDestDir, PATH_SEPARATOR, pFileHash->GetFilename());

daemon/postprocess/ParRenamer.h
@@ -71,6 +71,8 @@ private:
 	int					m_iFileCount;
 	int					m_iCurFile;
 	int					m_iRenamedCount;
+	int					m_iMissedCount;
+	int					m_iFoundCount;
 	bool				m_bHasSplittedFragments;
 
 	void				Cleanup();
@@ -104,6 +106,7 @@ public:
 	void				Cancel();
 	bool				GetCancelled() { return m_bCancelled; }
 	bool				HasSplittedFragments() { return m_bHasSplittedFragments; }
+	bool				HasMissedFiles() { return m_iMissedCount > 0; }
 };
 
 #endif

nzbget.conf
@@ -1053,11 +1053,12 @@ ParRepair=yes
 # issues. Please apply the patches to libpar2 and recompile it.
 ParScan=auto
 
-# Check for renamed files (yes, no).
+# Check for renamed and missing files (yes, no).
 #
 # Par-rename restores original file names using information stored
-# in par2-files. When enabled the par-rename is performed as the first
-# step of post-processing for every nzb-file.
+# in par2-files. It also detects missing files (files listed in
+# par2-files but not present on disk). When enabled the par-rename is
+# performed as the first step of post-processing for every nzb-file.
 #
 # Par-rename is very fast and is highly recommended, especially if
 # unpack is disabled.

commit 2c74b7a7d3e9c2f22a5cb88e756bdacbd802e830

fixed: par-rename initiated unnecessary par-check if option "InterDir" were not active (bug introduced in r1030)
git-svn-id: svn://svn.code.sf.net/p/nzbget/code/trunk@1039 b1b436ca-2b18-4f33-ab5d-510789358d54

daemon/postprocess/ParCoordinator.cpp
@@ -304,6 +304,7 @@ void ParCoordinator::StartParRenameJob(PostInfo* pPostInfo)
 	m_ParRenamer.SetPostInfo(pPostInfo);
 	m_ParRenamer.SetDestDir(szDestDir);
 	m_ParRenamer.SetInfoName(pPostInfo->GetNZBInfo()->GetName());
+	m_ParRenamer.SetDetectMissing(pPostInfo->GetNZBInfo()->GetUnpackStatus() == NZBInfo::usNone);
 	m_ParRenamer.PrintMessage(Message::mkInfo, "Checking renamed files for %s", pPostInfo->GetNZBInfo()->GetName());
 	pPostInfo->SetWorking(true);
 	m_ParRenamer.Start();

daemon/postprocess/ParRenamer.cpp
@@ -88,6 +88,7 @@ ParRenamer::ParRenamer()
 	m_bCancelled = false;
 	m_bHasSplittedFragments = false;
 	m_bHasMissedFiles = false;
+	m_bDetectMissing = false;
 }
 
 ParRenamer::~ParRenamer()
@@ -172,7 +173,11 @@ void ParRenamer::Run()
 		}
 
 		CheckFiles(szDestDir, false);
-		CheckMissing();
+
+		if (m_bDetectMissing)
+		{
+			CheckMissing();
+		}
 	}
 
 	if (m_bCancelled)

daemon/postprocess/ParRenamer.h
@@ -76,6 +76,7 @@ private:
 	int					m_iRenamedCount;
 	bool				m_bHasSplittedFragments;
 	bool				m_bHasMissedFiles;
+	bool				m_bDetectMissing;
 
 	void				Cleanup();
 	void				ClearHashList();
@@ -110,6 +111,7 @@ public:
 	bool				GetCancelled() { return m_bCancelled; }
 	bool				HasSplittedFragments() { return m_bHasSplittedFragments; }
 	bool				HasMissedFiles() { return m_bHasMissedFiles; }
+	void				SetDetectMissing(bool bDetectMissing) { m_bDetectMissing = bDetectMissing; }
 };
 
 #endif