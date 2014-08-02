ta264/nzbget_svn

commit bb0d0370de8e85539a695658de564efd94a6f7cf

added quick file verification during par-check/repair; if par-repair is required for download the files downloaded without errors are verified quickly by comparing their checksums against the checksums stored in the par2-file; this makes the verification of undamaged files almost instant; damaged files are verified as usual; new option "ParQuick" (active by default); added support for block-by-block scan of files during verification, which improves scan speed of damaged files; the quick par-verification requires a patch for libpar2 (see http://nzbget.net/libpar2 for details)
git-svn-id: svn://svn.code.sf.net/p/nzbget/code/trunk@1070 b1b436ca-2b18-4f33-ab5d-510789358d54

daemon/nntp/ArticleWriter.cpp
@@ -119,6 +119,7 @@ bool ArticleWriter::Start(Decoder::EFormat eFormat, const char* szFilename, int 
 	m_eFormat = eFormat;
 	m_iArticleOffset = iArticleOffset;
 	m_iArticleSize = iArticleSize ? iArticleSize : m_pArticleInfo->GetSize();
+	m_iArticlePtr = 0;
 
 	// prepare file for writing
 	if (m_eFormat == Decoder::efYenc)
@@ -168,7 +169,6 @@ bool ArticleWriter::Start(Decoder::EFormat eFormat, const char* szFilename, int 
 			g_pArticleCache->Free(m_iArticleSize);
 		}
 
-		m_iArticlePtr = 0;
 		m_pArticleData = (char*)g_pArticleCache->Alloc(m_iArticleSize);
 
 		while (!m_pArticleData && g_pArticleCache->GetFlushing())
@@ -206,15 +206,19 @@ bool ArticleWriter::Start(Decoder::EFormat eFormat, const char* szFilename, int 
 
 bool ArticleWriter::Write(char* szBufffer, int iLen)
 {
+	if (g_pOptions->GetDecode())
+	{
+		m_iArticlePtr += iLen;
+	}
+
 	if (g_pOptions->GetDecode() && m_pArticleData)
 	{
-		if (m_iArticlePtr + iLen > m_iArticleSize)
+		if (m_iArticlePtr > m_iArticleSize)
 		{
 			detail("Decoding %s failed: article size mismatch", m_szInfoName);
 			return false;
 		}
-		memcpy(m_pArticleData + m_iArticlePtr, szBufffer, iLen);
-		m_iArticlePtr += iLen;
+		memcpy(m_pArticleData + m_iArticlePtr - iLen, szBufffer, iLen);
 		return true;
 	}
 
@@ -263,6 +267,11 @@ void ArticleWriter::Finish(bool bSuccess)
 			m_pFileInfo->SetCachedArticles(m_pFileInfo->GetCachedArticles() + 1);
 			g_pArticleCache->UnlockContent();
 			m_pArticleData = NULL;
+		}
+		else
+		{
+			m_pArticleInfo->SetSegmentOffset(m_iArticleOffset);
+			m_pArticleInfo->SetSegmentSize(m_iArticlePtr);
 		}
 	}
 	else 
@@ -364,27 +373,27 @@ void ArticleWriter::CompleteFileParts()
 	szNZBName[1024-1] = '\0';
 	szNZBDestDir[1024-1] = '\0';
 	
-	char InfoFilename[1024];
-	snprintf(InfoFilename, 1024, "%s%c%s", szNZBName, (int)PATH_SEPARATOR, m_pFileInfo->GetFilename());
-	InfoFilename[1024-1] = '\0';
+	char szInfoFilename[1024];
+	snprintf(szInfoFilename, 1024, "%s%c%s", szNZBName, (int)PATH_SEPARATOR, m_pFileInfo->GetFilename());
+	szInfoFilename[1024-1] = '\0';
 
 	bool bCached = m_pFileInfo->GetCachedArticles() > 0;
 
 	if (!g_pOptions->GetDecode())
 	{
-		detail("Moving articles for %s", InfoFilename);
+		detail("Moving articles for %s", szInfoFilename);
 	}
 	else if (bDirectWrite && bCached)
 	{
-		detail("Writing articles for %s", InfoFilename);
+		detail("Writing articles for %s", szInfoFilename);
 	}
 	else if (bDirectWrite)
 	{
-		detail("Checking articles for %s", InfoFilename);
+		detail("Checking articles for %s", szInfoFilename);
 	}
 	else
 	{
-		detail("Joining articles for %s", InfoFilename);
+		detail("Joining articles for %s", szInfoFilename);
 	}
 
 	// Ensure the DstDir is created
@@ -444,10 +453,10 @@ void ArticleWriter::CompleteFileParts()
 		m_bFlushing = true;
 	}
 
-	bool complete = true;
-	int iBrokenCount = 0;
-	static const int BUFFER_SIZE = 1024 * 50;
+	static const int BUFFER_SIZE = 1024 * 64;
 	char* buffer = NULL;
+	bool bFirstArticle = true;
+	unsigned long lCrc = 0;
 
 	if (g_pOptions->GetDecode() && !bDirectWrite)
 	{
@@ -459,8 +468,6 @@ void ArticleWriter::CompleteFileParts()
 		ArticleInfo* pa = *it;
 		if (pa->GetStatus() != ArticleInfo::aiFinished)
 		{
-			iBrokenCount++;
-			complete = false;
 			continue;
 		}
 
@@ -469,7 +476,7 @@ void ArticleWriter::CompleteFileParts()
 		{
 			memset(buffer, 0, BUFFER_SIZE);
 			while (pa->GetSegmentOffset() > ftell(outfile) && ftell(outfile) > -1 &&
-				   fwrite(buffer, 1, std::min((int)(pa->GetSegmentOffset() - ftell(outfile)), BUFFER_SIZE), outfile)) ;
+				fwrite(buffer, 1, (std::min)((int)(pa->GetSegmentOffset() - ftell(outfile)), BUFFER_SIZE), outfile)) ;
 		}
 
 		if (pa->GetSegmentContent())
@@ -481,40 +488,40 @@ void ArticleWriter::CompleteFileParts()
 		}
 		else if (g_pOptions->GetDecode() && !bDirectWrite)
 		{
-			FILE* infile;
-			const char* fn = pa->GetResultFilename();
-
-			infile = fopen(fn, FOPEN_RB);
+			FILE* infile = fopen(pa->GetResultFilename(), FOPEN_RB);
 			if (infile)
 			{
 				int cnt = BUFFER_SIZE;
-
 				while (cnt == BUFFER_SIZE)
 				{
 					cnt = (int)fread(buffer, 1, BUFFER_SIZE, infile);
 					fwrite(buffer, 1, cnt, outfile);
 					SetLastUpdateTimeNow();
 				}
-
 				fclose(infile);
 			}
 			else
 			{
-				complete = false;
-				iBrokenCount++;
-				detail("Could not find file %s. Status is broken", fn);
+				m_pFileInfo->SetFailedArticles(m_pFileInfo->GetFailedArticles() + 1);
+				m_pFileInfo->SetSuccessArticles(m_pFileInfo->GetSuccessArticles() - 1);
+				detail("Could not find file %s. Status is broken", pa->GetResultFilename());
 			}
 		}
 		else if (!g_pOptions->GetDecode())
 		{
-			const char* fn = pa->GetResultFilename();
 			char dstFileName[1024];
 			snprintf(dstFileName, 1024, "%s%c%03i", ofn, (int)PATH_SEPARATOR, pa->GetPartNumber());
 			dstFileName[1024-1] = '\0';
-			if (!Util::MoveFile(fn, dstFileName))
+			if (!Util::MoveFile(pa->GetResultFilename(), dstFileName))
 			{
-				error("Could not move file %s to %s: %s", fn, dstFileName, Util::GetLastErrorMessage(szErrBuf, sizeof(szErrBuf)));
+				error("Could not move file %s to %s: %s", pa->GetResultFilename(), dstFileName, Util::GetLastErrorMessage(szErrBuf, sizeof(szErrBuf)));
 			}
+		}
+
+		if (m_eFormat == Decoder::efYenc)
+		{
+			lCrc = bFirstArticle ? pa->GetCrc() : Util::Crc32Combine(lCrc, pa->GetCrc(), pa->GetSegmentSize());
+			bFirstArticle = false;
 		}
 	}
 
@@ -570,15 +577,14 @@ void ArticleWriter::CompleteFileParts()
 		}
 	}
 
-	if (complete)
+	if (m_pFileInfo->GetMissedArticles() == 0 && m_pFileInfo->GetFailedArticles() == 0)
 	{
-		info("Successfully downloaded %s", InfoFilename);
+		info("Successfully downloaded %s", szInfoFilename);
 	}
 	else
 	{
-		warn("%i of %i article downloads failed for \"%s\"",
-			iBrokenCount + m_pFileInfo->GetMissedArticles(),
-			m_pFileInfo->GetTotalArticles(), InfoFilename);
+		warn("%i of %i article downloads failed for \"%s\"", m_pFileInfo->GetMissedArticles() + m_pFileInfo->GetFailedArticles(),
+			m_pFileInfo->GetTotalArticles(), szInfoFilename);
 
 		if (g_pOptions->GetCreateBrokenLog())
 		{
@@ -586,16 +592,22 @@ void ArticleWriter::CompleteFileParts()
 			snprintf(szBrokenLogName, 1024, "%s%c_brokenlog.txt", szNZBDestDir, (int)PATH_SEPARATOR);
 			szBrokenLogName[1024-1] = '\0';
 			FILE* file = fopen(szBrokenLogName, FOPEN_AB);
-			fprintf(file, "%s (%i/%i)%s", m_pFileInfo->GetFilename(),
-				m_pFileInfo->GetTotalArticles() - iBrokenCount - m_pFileInfo->GetMissedArticles(),
+			fprintf(file, "%s (%i/%i)%s", m_pFileInfo->GetFilename(), m_pFileInfo->GetSuccessArticles(),
 				m_pFileInfo->GetTotalArticles(), LINE_ENDING);
 			fclose(file);
 		}
+
+		lCrc = 0;
 	}
+
+	CompletedFile::EStatus eFileStatus = m_pFileInfo->GetMissedArticles() == 0 &&
+		m_pFileInfo->GetFailedArticles() == 0 ? CompletedFile::cfSuccess :
+		m_pFileInfo->GetSuccessArticles() > 0 ? CompletedFile::cfPartial :
+		CompletedFile::cfFailure;
 
 	// the locking is needed for accessing the members of NZBInfo
 	DownloadQueue::Lock();
-	m_pFileInfo->GetNZBInfo()->GetCompletedFiles()->push_back(strdup(ofn));
+	m_pFileInfo->GetNZBInfo()->GetCompletedFiles()->push_back(new CompletedFile(Util::BaseFileName(ofn), eFileStatus, lCrc));
 	if (strcmp(m_pFileInfo->GetNZBInfo()->GetDestDir(), szNZBDestDir))
 	{
 		// destination directory was changed during completion, need to move the file
@@ -606,14 +618,14 @@ void ArticleWriter::CompleteFileParts()
 
 void ArticleWriter::FlushCache()
 {
-	char InfoFilename[1024];
+	char szInfoFilename[1024];
 	// the locking is needed for accessing the members of NZBInfo
 	DownloadQueue::Lock();
-	snprintf(InfoFilename, 1024, "%s%c%s", m_pFileInfo->GetNZBInfo()->GetName(), (int)PATH_SEPARATOR, m_pFileInfo->GetFilename());
-	InfoFilename[1024-1] = '\0';
+	snprintf(szInfoFilename, 1024, "%s%c%s", m_pFileInfo->GetNZBInfo()->GetName(), (int)PATH_SEPARATOR, m_pFileInfo->GetFilename());
+	szInfoFilename[1024-1] = '\0';
 	DownloadQueue::Unlock();
 
-	detail("Flushing cache for %s", InfoFilename);
+	detail("Flushing cache for %s", szInfoFilename);
 
 	bool bDirectWrite = g_pOptions->GetDirectWrite() && m_pFileInfo->GetOutputInitialized();
 	FILE* outfile = NULL;
@@ -715,7 +727,7 @@ void ArticleWriter::FlushCache()
 
 	g_pArticleCache->UnlockFlush();
 
-	detail("Saved %i articles (%.2f MB) from cache into disk for %s", iFlushedArticles, (float)(iFlushedSize / 1024.0 / 1024.0), InfoFilename);
+	detail("Saved %i articles (%.2f MB) from cache into disk for %s", iFlushedArticles, (float)(iFlushedSize / 1024.0 / 1024.0), szInfoFilename);
 }
 
 bool ArticleWriter::MoveCompletedFiles(NZBInfo* pNZBInfo, const char* szOldDestDir)
@@ -734,29 +746,29 @@ bool ArticleWriter::MoveCompletedFiles(NZBInfo* pNZBInfo, const char* szOldDestD
 	}
 
 	// move already downloaded files to new destination
-	for (NZBInfo::Files::iterator it = pNZBInfo->GetCompletedFiles()->begin(); it != pNZBInfo->GetCompletedFiles()->end(); it++)
+	for (NZBInfo::CompletedFiles::iterator it = pNZBInfo->GetCompletedFiles()->begin(); it != pNZBInfo->GetCompletedFiles()->end(); it++)
     {
-		char* szFileName = *it;
+		CompletedFile* pCompletedFile = *it;
+
+		char szOldFileName[1024];
+		snprintf(szOldFileName, 1024, "%s%c%s", szOldDestDir, (int)PATH_SEPARATOR, pCompletedFile->GetFileName());
+		szOldFileName[1024-1] = '\0';
+
 		char szNewFileName[1024];
-		snprintf(szNewFileName, 1024, "%s%c%s", pNZBInfo->GetDestDir(), (int)PATH_SEPARATOR, Util::BaseFileName(szFileName));
+		snprintf(szNewFileName, 1024, "%s%c%s", pNZBInfo->GetDestDir(), (int)PATH_SEPARATOR, pCompletedFile->GetFileName());
 		szNewFileName[1024-1] = '\0';
 
 		// check if file was not moved already
-		if (strcmp(szFileName, szNewFileName))
+		if (strcmp(szOldDestDir, szNewFileName))
 		{
 			// prevent overwriting of existing files
-			Util::MakeUniqueFilename(szNewFileName, 1024, pNZBInfo->GetDestDir(), Util::BaseFileName(szFileName));
+			Util::MakeUniqueFilename(szNewFileName, 1024, pNZBInfo->GetDestDir(), pCompletedFile->GetFileName());
 
-			detail("Moving file %s to %s", szFileName, szNewFileName);
-			if (Util::MoveFile(szFileName, szNewFileName))
-			{
-				free(szFileName);
-				*it = strdup(szNewFileName);
-			}
-			else
+			detail("Moving file %s to %s", szOldDestDir, szNewFileName);
+			if (!Util::MoveFile(szOldDestDir, szNewFileName))
 			{
 				char szErrBuf[256];
-				error("Could not move file %s to %s: %s", szFileName, szNewFileName, Util::GetLastErrorMessage(szErrBuf, sizeof(szErrBuf)));
+				error("Could not move file %s to %s: %s", szOldDestDir, szNewFileName, Util::GetLastErrorMessage(szErrBuf, sizeof(szErrBuf)));
 			}
 		}
     }

config.h.in
@@ -67,11 +67,14 @@
 /* Define to 1 to use OpenSSL library for TLS/SSL-support. */
 #undef HAVE_OPENSSL
 
-/* Define to 1 if libpar2 has recent bugfixes-patch (version 2) */
-#undef HAVE_PAR2_BUGFIXES_V2
+/* Define to 1 if libpar2 supports block-scan (needs a special patch) */
+#undef HAVE_PAR2_BLOCKSCAN
 
 /* Define to 1 if libpar2 supports cancelling (needs a special patch) */
 #undef HAVE_PAR2_CANCEL
+
+/* Define to 1 if libpar2 supports external verification */
+#undef HAVE_PAR2_CRCPATCH
 
 /* Define to 1 if you have the <regex.h> header file. */
 #undef HAVE_REGEX_H

configure
@@ -1320,7 +1320,7 @@ Optional Features:
                           curses-library)
   --disable-parcheck      do not include par-check/-repair-support (removes
                           dependency from libpar2- and libsigc-libraries)
-  --disable-libpar2-bugfixes-check
+  --disable-libpar2-version-check
                           do not check libpar2 version
   --disable-tls           do not use TLS/SSL (removes dependency from
                           TLS/SSL-libraries)
@@ -7624,8 +7624,8 @@ fi
 
 rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
 
-				{ echo "$as_me:$LINENO: checking whether libpar2 has recent bugfixes" >&5
-echo $ECHO_N "checking whether libpar2 has recent bugfixes... $ECHO_C" >&6; }
+				{ echo "$as_me:$LINENO: checking whether libpar2 supports block-scan" >&5
+echo $ECHO_N "checking whether libpar2 supports block-scan... $ECHO_C" >&6; }
 	cat >conftest.$ac_ext <<_ACEOF
 /* confdefs.h.  */
 _ACEOF
@@ -7634,7 +7634,7 @@ cat >>conftest.$ac_ext <<_ACEOF
 /* end confdefs.h.  */
 #include <libpar2/par2cmdline.h>
 		#include <libpar2/par2repairer.h>
-		 class Repairer : public Par2Repairer { void test() { BugfixesPatchVersion2(); } };
+		 class Repairer : public Par2Repairer { void test() { blockscan = true; } };
 int
 main ()
 {
@@ -7662,10 +7662,9 @@ eval "echo \"\$as_me:$LINENO: $ac_try_echo\"") >&5
        } && test -s conftest.$ac_objext; then
   { echo "$as_me:$LINENO: result: yes" >&5
 echo "${ECHO_T}yes" >&6; }
-		PAR2PATCHV2=yes
 
 cat >>confdefs.h <<\_ACEOF
-#define HAVE_PAR2_BUGFIXES_V2 1
+#define HAVE_PAR2_BLOCKSCAN 1
 _ACEOF
 
 else
@@ -7674,22 +7673,78 @@ sed 's/^/| /' conftest.$ac_ext >&5
 
 	{ echo "$as_me:$LINENO: result: no" >&5
 echo "${ECHO_T}no" >&6; }
-		PAR2PATCHV2=no
 fi
 
 rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
 
-	if test "$PAR2PATCHV2" = "no" ; then
-		# Check whether --enable-libpar2-bugfixes-check was given.
-if test "${enable_libpar2_bugfixes_check+set}" = set; then
-  enableval=$enable_libpar2_bugfixes_check;  PAR2PATCHCHECK=$enableval
+				{ echo "$as_me:$LINENO: checking whether libpar2 supports external verification" >&5
+echo $ECHO_N "checking whether libpar2 supports external verification... $ECHO_C" >&6; }
+	cat >conftest.$ac_ext <<_ACEOF
+/* confdefs.h.  */
+_ACEOF
+cat confdefs.h >>conftest.$ac_ext
+cat >>conftest.$ac_ext <<_ACEOF
+/* end confdefs.h.  */
+#include <libpar2/par2cmdline.h>
+		#include <libpar2/par2repairer.h>
+		 class Repairer : public Par2Repairer { protected:
+	virtual int ScanDataFile(DiskFile *diskfile, Par2RepairerSourceFile* &sourcefile,
+		MatchType &matchtype, MD5Hash &hashfull, MD5Hash &hash16k, u32 &count) { return true; } };
+int
+main ()
+{
+
+  ;
+  return 0;
+}
+_ACEOF
+rm -f conftest.$ac_objext
+if { (ac_try="$ac_compile"
+case "(($ac_try" in
+  *\"* | *\`* | *\\*) ac_try_echo=\$ac_try;;
+  *) ac_try_echo=$ac_try;;
+esac
+eval "echo \"\$as_me:$LINENO: $ac_try_echo\"") >&5
+  (eval "$ac_compile") 2>conftest.er1
+  ac_status=$?
+  grep -v '^ *+' conftest.er1 >conftest.err
+  rm -f conftest.er1
+  cat conftest.err >&5
+  echo "$as_me:$LINENO: \$? = $ac_status" >&5
+  (exit $ac_status); } && {
+	 test -z "$ac_cxx_werror_flag" ||
+	 test ! -s conftest.err
+       } && test -s conftest.$ac_objext; then
+  { echo "$as_me:$LINENO: result: no" >&5
+echo "${ECHO_T}no" >&6; }
+		PAR2CRCPATCH=no
+
+cat >>confdefs.h <<\_ACEOF
+#define HAVE_PAR2_CRCPATCH 1
+_ACEOF
+
+else
+  echo "$as_me: failed program was:" >&5
+sed 's/^/| /' conftest.$ac_ext >&5
+
+	{ echo "$as_me:$LINENO: result: yes" >&5
+echo "${ECHO_T}yes" >&6; }
+		PAR2CRCPATCH=yes
+fi
+
+rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
+
+	if test "$PAR2CRCPATCH" = "no" ; then
+		# Check whether --enable-libpar2-version-check was given.
+if test "${enable_libpar2_version_check+set}" = set; then
+  enableval=$enable_libpar2_version_check;  PAR2PATCHCHECK=$enableval
 else
    PAR2PATCHCHECK=yes
 fi
 
 		if test "$PAR2PATCHCHECK" = "yes"; then
-			{ { echo "$as_me:$LINENO: error: Your version of libpar2 doesn't include the recent bugfixes. Please update libpar2 to version 0.4 or newer (http://launchpad.net/libpar2). If you cannot install a newer version of libpar2, you can use configure parameter --disable-libpar2-bugfixes-check to suppress the check. Please note however that in this case the program may crash during par-check/repair. The update is highly recommended!" >&5
-echo "$as_me: error: Your version of libpar2 doesn't include the recent bugfixes. Please update libpar2 to version 0.4 or newer (http://launchpad.net/libpar2). If you cannot install a newer version of libpar2, you can use configure parameter --disable-libpar2-bugfixes-check to suppress the check. Please note however that in this case the program may crash during par-check/repair. The update is highly recommended!" >&2;}
+			{ { echo "$as_me:$LINENO: error: Your version of libpar2 doesn't support external verification, which drastically improves verification time in nzbget. Please update your libpar2 as explained on http://nzbget.net/libpar2. If you cannot update libpar2, you can use configure parameter --disable-libpar2-version-check to suppress the check." >&5
+echo "$as_me: error: Your version of libpar2 doesn't support external verification, which drastically improves verification time in nzbget. Please update your libpar2 as explained on http://nzbget.net/libpar2. If you cannot update libpar2, you can use configure parameter --disable-libpar2-version-check to suppress the check." >&2;}
    { (exit 1); exit 1; }; }
 		fi
 	fi

configure.ac
@@ -336,27 +336,42 @@ if test "$ENABLEPARCHECK" = "yes"; then
 		AC_MSG_RESULT([[no]]))
 
 	dnl
-	dnl check if libpar2 has recent bugfixes-patch
+	dnl check if libpar2 has support for block-scan
 	dnl
-	AC_MSG_CHECKING(whether libpar2 has recent bugfixes)
+	AC_MSG_CHECKING(whether libpar2 supports block-scan)
 	AC_TRY_COMPILE(
 		[#include <libpar2/par2cmdline.h>]
 		[#include <libpar2/par2repairer.h>]
-		[ class Repairer : public Par2Repairer { void test() { BugfixesPatchVersion2(); } }; ],
+		[ class Repairer : public Par2Repairer { void test() { blockscan = true; } }; ],
 		[],
 		AC_MSG_RESULT([[yes]])
-		PAR2PATCHV2=yes
-		AC_DEFINE([HAVE_PAR2_BUGFIXES_V2], 1, [Define to 1 if libpar2 has recent bugfixes-patch (version 2)]),
-		AC_MSG_RESULT([[no]])
-		PAR2PATCHV2=no)
+		AC_DEFINE([HAVE_PAR2_BLOCKSCAN], 1, [Define to 1 if libpar2 supports block-scan (needs a special patch)]),
+		AC_MSG_RESULT([[no]]))
 
-	if test "$PAR2PATCHV2" = "no" ; then
-		AC_ARG_ENABLE(libpar2-bugfixes-check,
-			[AS_HELP_STRING([--disable-libpar2-bugfixes-check], [do not check libpar2 version])],
+	dnl
+	dnl check if libpar2 has external-verify-patch
+	dnl
+	AC_MSG_CHECKING(whether libpar2 supports external verification)
+	AC_TRY_COMPILE(
+		[#include <libpar2/par2cmdline.h>]
+		[#include <libpar2/par2repairer.h>]
+		[ class Repairer : public Par2Repairer { protected:
+	virtual int ScanDataFile(DiskFile *diskfile, Par2RepairerSourceFile* &sourcefile,
+		MatchType &matchtype, MD5Hash &hashfull, MD5Hash &hash16k, u32 &count) { return true; } }; ],
+		[],
+		AC_MSG_RESULT([[no]])
+		PAR2CRCPATCH=no
+		AC_DEFINE([HAVE_PAR2_CRCPATCH], 1, [Define to 1 if libpar2 supports external verification]),
+		AC_MSG_RESULT([[yes]])
+		PAR2CRCPATCH=yes)
+
+	if test "$PAR2CRCPATCH" = "no" ; then
+		AC_ARG_ENABLE(libpar2-version-check,
+			[AS_HELP_STRING([--disable-libpar2-version-check], [do not check libpar2 version])],
 			[ PAR2PATCHCHECK=$enableval ],
 			[ PAR2PATCHCHECK=yes] )
 		if test "$PAR2PATCHCHECK" = "yes"; then
-			AC_ERROR([Your version of libpar2 doesn't include the recent bugfixes. Please update libpar2 to version 0.4 or newer (http://launchpad.net/libpar2). If you cannot install a newer version of libpar2, you can use configure parameter --disable-libpar2-bugfixes-check to suppress the check. Please note however that in this case the program may crash during par-check/repair. The update is highly recommended!])
+			AC_ERROR([Your version of libpar2 doesn't support external verification, which drastically improves verification time in nzbget. Please update your libpar2 as explained on http://nzbget.net/libpar2. If you cannot update libpar2, you can use configure parameter --disable-libpar2-version-check to suppress the check.])
 		fi
 	fi
 

daemon/main/Options.cpp
@@ -144,6 +144,7 @@ static const char* OPTION_DETAILTARGET			= "DetailTarget";
 static const char* OPTION_PARCHECK				= "ParCheck";
 static const char* OPTION_PARREPAIR				= "ParRepair";
 static const char* OPTION_PARSCAN				= "ParScan";
+static const char* OPTION_PARQUICK				= "ParQuick";
 static const char* OPTION_PARRENAME				= "ParRename";
 static const char* OPTION_HEALTHCHECK			= "HealthCheck";
 static const char* OPTION_SCANSCRIPT			= "ScanScript";
@@ -503,6 +504,7 @@ Options::Options(int argc, char* argv[])
 	m_eParCheck				= pcManual;
 	m_bParRepair			= false;
 	m_eParScan				= psLimited;
+	m_bParQuick				= true;
 	m_bParRename			= false;
 	m_eHealthCheck			= hcNone;
 	m_szScriptOrder			= NULL;
@@ -770,6 +772,7 @@ void Options::InitDefault()
 	SetOption(OPTION_PARCHECK, "auto");
 	SetOption(OPTION_PARREPAIR, "yes");
 	SetOption(OPTION_PARSCAN, "limited");
+	SetOption(OPTION_PARQUICK, "yes");
 	SetOption(OPTION_PARRENAME, "yes");
 	SetOption(OPTION_HEALTHCHECK, "none");
 	SetOption(OPTION_SCRIPTORDER, "");
@@ -989,6 +992,7 @@ void Options::InitOptions()
 	m_bSaveQueue			= (bool)ParseEnumValue(OPTION_SAVEQUEUE, BoolCount, BoolNames, BoolValues);
 	m_bDupeCheck			= (bool)ParseEnumValue(OPTION_DUPECHECK, BoolCount, BoolNames, BoolValues);
 	m_bParRepair			= (bool)ParseEnumValue(OPTION_PARREPAIR, BoolCount, BoolNames, BoolValues);
+	m_bParQuick				= (bool)ParseEnumValue(OPTION_PARQUICK, BoolCount, BoolNames, BoolValues);
 	m_bParRename			= (bool)ParseEnumValue(OPTION_PARRENAME, BoolCount, BoolNames, BoolValues);
 	m_bReloadQueue			= (bool)ParseEnumValue(OPTION_RELOADQUEUE, BoolCount, BoolNames, BoolValues);
 	m_bCursesNZBName		= (bool)ParseEnumValue(OPTION_CURSESNZBNAME, BoolCount, BoolNames, BoolValues);

daemon/main/Options.h
@@ -287,6 +287,7 @@ private:
 	EParCheck			m_eParCheck;
 	bool				m_bParRepair;
 	EParScan			m_eParScan;
+	bool				m_bParQuick;
 	bool				m_bParRename;
 	EHealthCheck		m_eHealthCheck;
 	char*				m_szPostScript;
@@ -454,6 +455,7 @@ public:
 	EParCheck			GetParCheck() { return m_eParCheck; }
 	bool				GetParRepair() { return m_bParRepair; }
 	EParScan			GetParScan() { return m_eParScan; }
+	bool				GetParQuick() { return m_bParQuick; }
 	bool				GetParRename() { return m_bParRename; }
 	EHealthCheck		GetHealthCheck() { return m_eHealthCheck; }
 	const char*			GetScriptOrder() { return m_szScriptOrder; }

daemon/main/nzbget.h
@@ -50,7 +50,6 @@
 #define	S_ISREG(mode)	 __S_ISTYPE((mode), _S_IFREG)
 #define	S_DIRMODE NULL
 #define usleep(usec) Sleep((usec) / 1000)
-#define gettimeofday(tm, ignore) _ftime(tm)
 #define socklen_t int
 #define SHUT_WR 0x01
 #define SHUT_RDWR 0x02

daemon/nntp/ArticleDownloader.cpp
@@ -327,6 +327,7 @@ ArticleDownloader::EStatus ArticleDownloader::Download()
 	const char* szResponse = NULL;
 	EStatus Status = adRunning;
 	m_bWritingStarted = false;
+	m_pArticleInfo->SetCrc(0);
 
 	if (m_pConnection->GetNewsServer()->GetJoinGroup())
 	{
@@ -612,6 +613,12 @@ ArticleDownloader::EStatus ArticleDownloader::DecodeCheck()
 				free(m_szArticleFilename);
 				m_szArticleFilename = strdup(pDecoder->GetArticleFilename());
 			}
+
+			if (m_eFormat == Decoder::efYenc)
+			{
+				m_pArticleInfo->SetCrc(m_YDecoder.GetExpectedCrc());
+			}
+
 			return adFinished;
 		}
 		else if (eStatus == Decoder::eCrcError)

commit 649f8f7471feb870bb1fb86124753634c660d5e8

fixed: renaming of active downloads was broken (bug introduced in r1070)
git-svn-id: svn://svn.code.sf.net/p/nzbget/code/trunk@1072 b1b436ca-2b18-4f33-ab5d-510789358d54

daemon/nntp/ArticleWriter.cpp
@@ -759,16 +759,16 @@ bool ArticleWriter::MoveCompletedFiles(NZBInfo* pNZBInfo, const char* szOldDestD
 		szNewFileName[1024-1] = '\0';
 
 		// check if file was not moved already
-		if (strcmp(szOldDestDir, szNewFileName))
+		if (strcmp(szOldFileName, szNewFileName))
 		{
 			// prevent overwriting of existing files
 			Util::MakeUniqueFilename(szNewFileName, 1024, pNZBInfo->GetDestDir(), pCompletedFile->GetFileName());
 
-			detail("Moving file %s to %s", szOldDestDir, szNewFileName);
-			if (!Util::MoveFile(szOldDestDir, szNewFileName))
+			detail("Moving file %s to %s", szOldFileName, szNewFileName);
+			if (!Util::MoveFile(szOldFileName, szNewFileName))
 			{
 				char szErrBuf[256];
-				error("Could not move file %s to %s: %s", szOldDestDir, szNewFileName, Util::GetLastErrorMessage(szErrBuf, sizeof(szErrBuf)));
+				error("Could not move file %s to %s: %s", szOldFileName, szNewFileName, Util::GetLastErrorMessage(szErrBuf, sizeof(szErrBuf)));
 			}
 		}
     }
@@ -835,6 +835,19 @@ bool ArticleWriter::MoveCompletedFiles(NZBInfo* pNZBInfo, const char* szOldDestD
 	// delete old directory (if empty)
 	if (Util::DirEmpty(szOldDestDir))
 	{
+		// check if there are pending writes into directory
+		for (FileList::iterator it = pNZBInfo->GetFileList()->begin(); it != pNZBInfo->GetFileList()->end(); it++)
+		{
+			FileInfo* pFileInfo = *it;
+			pFileInfo->LockOutputFile();
+			if (pFileInfo->GetOutputInitialized())
+			{
+				pFileInfo->UnlockOutputFile();
+				return true;
+			}
+			pFileInfo->UnlockOutputFile();
+		}
+
 		rmdir(szOldDestDir);
 	}
 