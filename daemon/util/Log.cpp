ta264/nzbget_svn

commit 8ce1e2a29b6533628c673a3fc1a9898d7d880023

added log file rotation; options "CreateLog" and "ResetLog" replaced with new option "WriteLog (none, append, reset, rotate)"; new option "RotateLog" defines rotation period; when compiled in debug mode new field "process id" is printed to the file log for each row (it is easier to identify processes than threads)
git-svn-id: svn://svn.code.sf.net/p/nzbget/code/trunk@1059 b1b436ca-2b18-4f33-ab5d-510789358d54

daemon/util/Log.cpp
@@ -2,7 +2,7 @@
  *  This file is part of nzbget
  *
  *  Copyright (C) 2004 Sven Henkel <sidddy@users.sourceforge.net>
- *  Copyright (C) 2007-2013 Andrey Prygunkov <hugbug@users.sourceforge.net>
+ *  Copyright (C) 2007-2014 Andrey Prygunkov <hugbug@users.sourceforge.net>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -32,6 +32,7 @@
 #include "win32.h"
 #else
 #include <pthread.h>
+#include <unistd.h>
 #endif
 
 #include <stdlib.h>
@@ -52,6 +53,7 @@ Log::Log()
 	m_Messages.clear();
 	m_iIDGen = 0;
 	m_szLogFilename = NULL;
+	m_tLastWritten = 0;
 #ifdef DEBUG
 	m_bExtraDebug = Util::FileExists("extradebug");
 #endif
@@ -82,48 +84,57 @@ void Log::LogDebugInfo()
 
 void Log::Filelog(const char* msg, ...)
 {
-	if (m_szLogFilename)
+	if (!m_szLogFilename)
 	{
-		char tmp2[1024];
+		return;
+	}
 
-		va_list ap;
-		va_start(ap, msg);
-		vsnprintf(tmp2, 1024, msg, ap);
-		tmp2[1024-1] = '\0';
-		va_end(ap);
+	char tmp2[1024];
 
-		time_t rawtime;
-		time(&rawtime);
-		rawtime += g_pOptions->GetTimeCorrection();
-		
-		char szTime[50];
+	va_list ap;
+	va_start(ap, msg);
+	vsnprintf(tmp2, 1024, msg, ap);
+	tmp2[1024-1] = '\0';
+	va_end(ap);
+
+	time_t rawtime = time(NULL) + g_pOptions->GetTimeCorrection();
+	
+	char szTime[50];
 #ifdef HAVE_CTIME_R_3
-		ctime_r(&rawtime, szTime, 50);
+	ctime_r(&rawtime, szTime, 50);
 #else
-		ctime_r(&rawtime, szTime);
+	ctime_r(&rawtime, szTime);
 #endif
-		szTime[50-1] = '\0';
-		szTime[strlen(szTime) - 1] = '\0'; // trim LF
+	szTime[50-1] = '\0';
+	szTime[strlen(szTime) - 1] = '\0'; // trim LF
 
-		FILE* file = fopen(m_szLogFilename, FOPEN_ABP);
-		if (file)
-		{
+	if ((int)rawtime/86400 != (int)m_tLastWritten/86400 && g_pOptions->GetWriteLog() == Options::wlRotate)
+	{
+		RotateLog();
+	}
+
+	m_tLastWritten = rawtime;
+
+	FILE* file = fopen(m_szLogFilename, FOPEN_ABP);
+	if (file)
+	{
 #ifdef WIN32
-			unsigned long iThreadId = GetCurrentThreadId();
+		unsigned long iProcessId = GetCurrentProcessId();
+		unsigned long iThreadId = GetCurrentThreadId();
 #else
-			unsigned long iThreadId = (unsigned long)pthread_self();
+		unsigned long iProcessId = (unsigned long)getpid();
+		unsigned long iThreadId = (unsigned long)pthread_self();
 #endif
 #ifdef DEBUG
-			fprintf(file, "%s\t%lu\t%s%s", szTime, iThreadId, tmp2, LINE_ENDING);
+		fprintf(file, "%s\t%lu\t%lu\t%s%s", szTime, iProcessId, iThreadId, tmp2, LINE_ENDING);
 #else
-			fprintf(file, "%s\t%s%s", szTime, tmp2, LINE_ENDING);
+		fprintf(file, "%s\t%s%s", szTime, tmp2, LINE_ENDING);
 #endif
-			fclose(file);
-		}
-		else
-		{
-			perror(m_szLogFilename);
-		}
+		fclose(file);
+	}
+	else
+	{
+		perror(m_szLogFilename);
 	}
 }
 
@@ -166,13 +177,13 @@ void debug(const char* msg, ...)
 	}
 
 	Options::EMessageTarget eMessageTarget = g_pOptions ? g_pOptions->GetDebugTarget() : Options::mtScreen;
-	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
-	{
-		g_pLog->Filelog("DEBUG\t%s", tmp2);
-	}
 	if (eMessageTarget == Options::mtScreen || eMessageTarget == Options::mtBoth)
 	{
 		g_pLog->AppendMessage(Message::mkDebug, tmp2);
+	}
+	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
+	{
+		g_pLog->Filelog("DEBUG\t%s", tmp2);
 	}
 
 	g_pLog->m_mutexLog.Unlock();
@@ -192,13 +203,13 @@ void error(const char* msg, ...)
 	g_pLog->m_mutexLog.Lock();
 
 	Options::EMessageTarget eMessageTarget = g_pOptions ? g_pOptions->GetErrorTarget() : Options::mtBoth;
-	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
-	{
-		g_pLog->Filelog("ERROR\t%s", tmp2);
-	}
 	if (eMessageTarget == Options::mtScreen || eMessageTarget == Options::mtBoth)
 	{
 		g_pLog->AppendMessage(Message::mkError, tmp2);
+	}
+	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
+	{
+		g_pLog->Filelog("ERROR\t%s", tmp2);
 	}
 
 	g_pLog->m_mutexLog.Unlock();
@@ -217,13 +228,13 @@ void warn(const char* msg, ...)
 	g_pLog->m_mutexLog.Lock();
 
 	Options::EMessageTarget eMessageTarget = g_pOptions ? g_pOptions->GetWarningTarget() : Options::mtScreen;
-	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
-	{
-		g_pLog->Filelog("WARNING\t%s", tmp2);
-	}
 	if (eMessageTarget == Options::mtScreen || eMessageTarget == Options::mtBoth)
 	{
 		g_pLog->AppendMessage(Message::mkWarning, tmp2);
+	}
+	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
+	{
+		g_pLog->Filelog("WARNING\t%s", tmp2);
 	}
 
 	g_pLog->m_mutexLog.Unlock();
@@ -242,13 +253,13 @@ void info(const char* msg, ...)
 	g_pLog->m_mutexLog.Lock();
 
 	Options::EMessageTarget eMessageTarget = g_pOptions ? g_pOptions->GetInfoTarget() : Options::mtScreen;
-	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
-	{
-		g_pLog->Filelog("INFO\t%s", tmp2);
-	}
 	if (eMessageTarget == Options::mtScreen || eMessageTarget == Options::mtBoth)
 	{
 		g_pLog->AppendMessage(Message::mkInfo, tmp2);
+	}
+	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
+	{
+		g_pLog->Filelog("INFO\t%s", tmp2);
 	}
 
 	g_pLog->m_mutexLog.Unlock();
@@ -267,13 +278,13 @@ void detail(const char* msg, ...)
 	g_pLog->m_mutexLog.Lock();
 
 	Options::EMessageTarget eMessageTarget = g_pOptions ? g_pOptions->GetDetailTarget() : Options::mtScreen;
-	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
-	{
-		g_pLog->Filelog("DETAIL\t%s", tmp2);
-	}
 	if (eMessageTarget == Options::mtScreen || eMessageTarget == Options::mtBoth)
 	{
 		g_pLog->AppendMessage(Message::mkDetail, tmp2);
+	}
+	if (eMessageTarget == Options::mtLog || eMessageTarget == Options::mtBoth)
+	{
+		g_pLog->Filelog("DETAIL\t%s", tmp2);
 	}
 
 	g_pLog->m_mutexLog.Unlock();
@@ -366,6 +377,82 @@ void Log::ResetLog()
 	remove(g_pOptions->GetLogFile());
 }
 
+void Log::RotateLog()
+{
+	char szDirectory[1024];
+	strncpy(szDirectory, g_pOptions->GetLogFile(), 1024);
+	szDirectory[1024-1] = '\0';
+
+	// split the full filename into path, basename and extension
+	char* szBaseName = Util::BaseFileName(szDirectory);
+	if (szBaseName > szDirectory)
+	{
+		szBaseName[-1] = '\0';
+	}
+
+	char szBaseExt[250];
+	char* szExt = strrchr(szBaseName, '.');
+	if (szExt && szExt > szBaseName)
+	{
+		strncpy(szBaseExt, szExt, 250);
+		szBaseExt[250-1] = '\0';
+		szExt[0] = '\0';
+	}
+	else
+	{
+		szBaseExt[0] = '\0';
+	}
+
+	char szFileMask[1024];
+	snprintf(szFileMask, 1024, "%s-####-##-##%s", szBaseName, szBaseExt);
+	szFileMask[1024-1] = '\0';
+
+	time_t tCurTime = time(NULL);
+	int iCurDay = (int)tCurTime / 86400;
+	char szFullFilename[1024];
+
+	WildMask mask(szFileMask, true);
+	DirBrowser dir(szDirectory);
+	while (const char* filename = dir.Next())
+	{
+		if (mask.Match(filename))
+		{
+			snprintf(szFullFilename, 1024, "%s%c%s", szDirectory, PATH_SEPARATOR, filename);
+			szFullFilename[1024-1] = '\0';
+
+			struct tm tm;
+			memset(&tm, 0, sizeof(tm));
+			tm.tm_year = atoi(filename + mask.GetMatchStart(0)) - 1900;
+			tm.tm_mon = atoi(filename + mask.GetMatchStart(1)) - 1;
+			tm.tm_mday = atoi(filename + mask.GetMatchStart(2));
+			time_t tFileTime = Util::Timegm(&tm);
+			int iFileDay = (int)tFileTime / 86400;
+
+			if (iFileDay <= iCurDay - g_pOptions->GetRotateLog())
+			{
+				char szMessage[1024];
+				snprintf(szMessage, 1024, "Deleting old log-file %s\n", filename);
+				szMessage[1024-1] = '\0';
+				g_pLog->AppendMessage(Message::mkInfo, szMessage);
+
+				remove(szFullFilename);
+			}
+		}
+	}
+
+	struct tm tm;
+	gmtime_r(&tCurTime, &tm);
+	snprintf(szFullFilename, 1024, "%s%c%s-%i-%.2i-%.2i%s", szDirectory, PATH_SEPARATOR,
+		szBaseName, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, szBaseExt);
+	szFullFilename[1024-1] = '\0';
+
+	free(m_szLogFilename);
+	m_szLogFilename = strdup(szFullFilename);
+#ifdef WIN32
+	WebUtil::Utf8ToAnsi(m_szLogFilename, strlen(m_szLogFilename) + 1);
+#endif
+}
+
 /*
 * During intializing stage (when options were not read yet) all messages
 * are saved in screen log, even if they shouldn't (according to options).
@@ -379,12 +466,17 @@ void Log::InitOptions()
 {
 	const char* szMessageType[] = { "INFO", "WARNING", "ERROR", "DEBUG", "DETAIL"};
 
-	if (g_pOptions->GetCreateLog() && g_pOptions->GetLogFile())
+	if (g_pOptions->GetWriteLog() != Options::wlNone && g_pOptions->GetLogFile())
 	{
 		m_szLogFilename = strdup(g_pOptions->GetLogFile());
 #ifdef WIN32
 		WebUtil::Utf8ToAnsi(m_szLogFilename, strlen(m_szLogFilename) + 1);
 #endif
+
+		if (g_pOptions->GetServerMode() && g_pOptions->GetWriteLog() == Options::wlReset)
+		{
+			g_pLog->ResetLog();
+		}
 	}
 
 	m_iIDGen = 0;

daemon/main/Options.cpp
@@ -107,8 +107,9 @@ static const char* OPTION_NZBDIR				= "NzbDir";
 static const char* OPTION_WEBDIR				= "WebDir";
 static const char* OPTION_CONFIGTEMPLATE		= "ConfigTemplate";
 static const char* OPTION_SCRIPTDIR				= "ScriptDir";
-static const char* OPTION_CREATELOG				= "CreateLog";
 static const char* OPTION_LOGFILE				= "LogFile";
+static const char* OPTION_WRITELOG				= "WriteLog";
+static const char* OPTION_ROTATELOG				= "RotateLog";
 static const char* OPTION_APPENDCATEGORYDIR		= "AppendCategoryDir";
 static const char* OPTION_LOCKFILE				= "LockFile";
 static const char* OPTION_DAEMONUSERNAME		= "DaemonUsername";
@@ -128,7 +129,6 @@ static const char* OPTION_CONNECTIONTIMEOUT		= "ConnectionTimeout";
 static const char* OPTION_SAVEQUEUE				= "SaveQueue";
 static const char* OPTION_RELOADQUEUE			= "ReloadQueue";
 static const char* OPTION_CREATEBROKENLOG		= "CreateBrokenLog";
-static const char* OPTION_RESETLOG				= "ResetLog";
 static const char* OPTION_DECODE				= "Decode";
 static const char* OPTION_RETRIES				= "Retries";
 static const char* OPTION_RETRYINTERVAL			= "RetryInterval";
@@ -200,6 +200,8 @@ static const char* OPTION_RELOADURLQUEUE		= "ReloadUrlQueue";
 static const char* OPTION_RELOADPOSTQUEUE		= "ReloadPostQueue";
 static const char* OPTION_NZBPROCESS			= "NZBProcess";
 static const char* OPTION_NZBADDEDPROCESS		= "NZBAddedProcess";
+static const char* OPTION_CREATELOG				= "CreateLog";
+static const char* OPTION_RESETLOG				= "ResetLog";
 
 const char* BoolNames[] = { "yes", "no", "true", "false", "1", "0", "on", "off", "enable", "disable", "enabled", "disabled" };
 const int BoolValues[] = { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 };
@@ -453,7 +455,6 @@ Options::Options(int argc, char* argv[])
 	m_bPauseScan			= false;
 	m_bTempPauseDownload	= false;
 	m_bCreateBrokenLog		= false;
-	m_bResetLog				= false;
 	m_iDownloadRate			= 0;
 	m_iEditQueueAction		= 0;
 	m_pEditQueueIDList		= NULL;
@@ -496,7 +497,8 @@ Options::Options(int argc, char* argv[])
 	m_iLogBufferSize		= 0;
 	m_iLogLines				= 0;
 	m_iWriteLogKind			= 0;
-	m_bCreateLog			= false;
+	m_eWriteLog				= wlAppend;
+	m_iRotateLog			= 0;
 	m_szLogFile				= NULL;
 	m_eParCheck				= pcManual;
 	m_bParRepair			= false;
@@ -734,7 +736,8 @@ void Options::InitDefault()
 	SetOption(OPTION_WEBDIR, "");
 	SetOption(OPTION_CONFIGTEMPLATE, "");
 	SetOption(OPTION_SCRIPTDIR, "${MainDir}/scripts");
-	SetOption(OPTION_CREATELOG, "yes");
+	SetOption(OPTION_WRITELOG, "append");
+	SetOption(OPTION_ROTATELOG, "3");
 	SetOption(OPTION_APPENDCATEGORYDIR, "yes");
 	SetOption(OPTION_OUTPUTMODE, "curses");
 	SetOption(OPTION_DUPECHECK, "yes");
@@ -752,7 +755,6 @@ void Options::InitDefault()
 	SetOption(OPTION_SAVEQUEUE, "yes");
 	SetOption(OPTION_RELOADQUEUE, "yes");
 	SetOption(OPTION_CREATEBROKENLOG, "yes");
-	SetOption(OPTION_RESETLOG, "no");
 	SetOption(OPTION_DECODE, "yes");
 	SetOption(OPTION_RETRIES, "3");
 	SetOption(OPTION_RETRYINTERVAL, "10");
@@ -960,6 +962,7 @@ void Options::InitOptions()
 	m_iSecurePort			= ParseIntValue(OPTION_SECUREPORT, 10);
 	m_iUrlConnections		= ParseIntValue(OPTION_URLCONNECTIONS, 10);
 	m_iLogBufferSize		= ParseIntValue(OPTION_LOGBUFFERSIZE, 10);
+	m_iRotateLog			= ParseIntValue(OPTION_ROTATELOG, 10);
 	m_iUMask				= ParseIntValue(OPTION_UMASK, 8);
 	m_iUpdateInterval		= ParseIntValue(OPTION_UPDATEINTERVAL, 10);
 	m_iWriteBuffer			= ParseIntValue(OPTION_WRITEBUFFER, 10);
@@ -981,12 +984,10 @@ void Options::InitOptions()
 	CheckDir(&m_szNzbDir, OPTION_NZBDIR, m_iNzbDirInterval == 0, true);
 
 	m_bCreateBrokenLog		= (bool)ParseEnumValue(OPTION_CREATEBROKENLOG, BoolCount, BoolNames, BoolValues);
-	m_bResetLog				= (bool)ParseEnumValue(OPTION_RESETLOG, BoolCount, BoolNames, BoolValues);
 	m_bAppendCategoryDir	= (bool)ParseEnumValue(OPTION_APPENDCATEGORYDIR, BoolCount, BoolNames, BoolValues);
 	m_bContinuePartial		= (bool)ParseEnumValue(OPTION_CONTINUEPARTIAL, BoolCount, BoolNames, BoolValues);
 	m_bSaveQueue			= (bool)ParseEnumValue(OPTION_SAVEQUEUE, BoolCount, BoolNames, BoolValues);
 	m_bDupeCheck			= (bool)ParseEnumValue(OPTION_DUPECHECK, BoolCount, BoolNames, BoolValues);
-	m_bCreateLog			= (bool)ParseEnumValue(OPTION_CREATELOG, BoolCount, BoolNames, BoolValues);
 	m_bParRepair			= (bool)ParseEnumValue(OPTION_PARREPAIR, BoolCount, BoolNames, BoolValues);
 	m_bParRename			= (bool)ParseEnumValue(OPTION_PARRENAME, BoolCount, BoolNames, BoolValues);
 	m_bReloadQueue			= (bool)ParseEnumValue(OPTION_RELOADQUEUE, BoolCount, BoolNames, BoolValues);
@@ -1037,6 +1038,11 @@ void Options::InitOptions()
 	m_eErrorTarget = (EMessageTarget)ParseEnumValue(OPTION_ERRORTARGET, TargetCount, TargetNames, TargetValues);
 	m_eDebugTarget = (EMessageTarget)ParseEnumValue(OPTION_DEBUGTARGET, TargetCount, TargetNames, TargetValues);
 	m_eDetailTarget = (EMessageTarget)ParseEnumValue(OPTION_DETAILTARGET, TargetCount, TargetNames, TargetValues);
+
+	const char* WriteLogNames[] = { "none", "append", "reset", "rotate" };
+	const int WriteLogValues[] = { wlNone, wlAppend, wlReset, wlRotate };
+	const int WriteLogCount = 4;
+	m_eWriteLog = (EWriteLog)ParseEnumValue(OPTION_WRITELOG, WriteLogCount, WriteLogNames, WriteLogValues);
 }
 
 int Options::ParseEnumValue(const char* OptName, int argc, const char * argn[], const int argv[])
@@ -2596,6 +2602,12 @@ bool Options::ValidateOptionName(const char* optname, const char* optvalue)
 				!strcasecmp(optname, OPTION_NZBADDEDPROCESS) ? OPTION_QUEUESCRIPT :
 				"ERROR");
 		}
+		return true;
+	}
+
+	if (!strcasecmp(optname, OPTION_CREATELOG) || !strcasecmp(optname, OPTION_RESETLOG))
+	{
+		ConfigWarn("Option \"%s\" is obsolete, ignored, use \"%s\" instead", optname, OPTION_WRITELOG);
 		return true;
 	}
 

daemon/main/Options.h
@@ -64,6 +64,13 @@ public:
 		opClientRequestHistory,
 		opClientRequestDownloadUrl
 	};
+	enum EWriteLog
+	{
+		wlNone,
+		wlAppend,
+		wlReset,
+		wlRotate
+	};
 	enum EMessageTarget
 	{
 		mtNone,
@@ -251,7 +258,6 @@ private:
 	EMessageTarget		m_eDetailTarget;
 	bool				m_bDecode;
 	bool				m_bCreateBrokenLog;
-	bool				m_bResetLog;
 	int					m_iConnectionTimeout;
 	int					m_iTerminateTimeout;
 	bool				m_bAppendCategoryDir;
@@ -275,7 +281,8 @@ private:
 	bool				m_bReloadQueue;
 	int					m_iUrlConnections;
 	int					m_iLogBufferSize;
-	bool				m_bCreateLog;
+	EWriteLog			m_eWriteLog;
+	int					m_iRotateLog;
 	char*				m_szLogFile;
 	EParCheck			m_eParCheck;
 	bool				m_bParRepair;
@@ -412,7 +419,6 @@ public:
 	const char*			GetConfigTemplate() { return m_szConfigTemplate; }
 	const char*			GetScriptDir() { return m_szScriptDir; }
 	bool				GetCreateBrokenLog() const { return m_bCreateBrokenLog; }
-	bool				GetResetLog() const { return m_bResetLog; }
 	EMessageTarget		GetInfoTarget() const { return m_eInfoTarget; }
 	EMessageTarget		GetWarningTarget() const { return m_eWarningTarget; }
 	EMessageTarget		GetErrorTarget() const { return m_eErrorTarget; }
@@ -442,8 +448,9 @@ public:
 	bool				GetReloadQueue() { return m_bReloadQueue; }
 	int					GetUrlConnections() { return m_iUrlConnections; }
 	int					GetLogBufferSize() { return m_iLogBufferSize; }
-	bool				GetCreateLog() { return m_bCreateLog; }
+	EWriteLog			GetWriteLog() { return m_eWriteLog; }
 	const char*			GetLogFile() { return m_szLogFile; }
+	int					GetRotateLog() { return m_iRotateLog; }
 	EParCheck			GetParCheck() { return m_eParCheck; }
 	bool				GetParRepair() { return m_bParRepair; }
 	EParScan			GetParScan() { return m_eParScan; }

daemon/main/nzbget.cpp
@@ -239,12 +239,6 @@ void Run(bool bReload)
 	}
 #endif
 	
-	if (g_pOptions->GetServerMode() && g_pOptions->GetCreateLog() && g_pOptions->GetResetLog())
-	{
-		debug("Deleting old log-file");
-		g_pLog->ResetLog();
-	}
-
 	g_pLog->InitOptions();
 	g_pScanner->InitOptions();
 

daemon/util/Log.h
@@ -96,12 +96,14 @@ private:
 	Mutex				m_mutexDebug;
 	char*				m_szLogFilename;
 	unsigned int		m_iIDGen;
+	time_t				m_tLastWritten;
 #ifdef DEBUG
 	bool				m_bExtraDebug;
 #endif
 
 	void				Filelog(const char* msg, ...);
 	void				AppendMessage(Message::EKind eKind, const char* szText);
+	void				RotateLog();
 
 	friend void error(const char* msg, ...);
 	friend void warn(const char* msg, ...);

nzbget.conf
@@ -91,7 +91,7 @@ LockFile=${MainDir}/nzbget.lock
 
 # Where to store log file, if it needs to be created.
 #
-# NOTE: See also option <CreateLog>.
+# NOTE: See also option <WriteLog>.
 LogFile=${DestDir}/nzbget.log
 
 # Configuration file template.
@@ -837,11 +837,20 @@ UrlForce=yes
 ##############################################################################
 ### LOGGING                                                                ###
 
-# Create log file (yes, no).
-CreateLog=yes
+# How to use log file (none, append, reset, rotate).
+#
+#  none   - do not write into log file;
+#  append - append to the existing log file or create it;
+#  reset  - delete existing log file on program start and create a new one;
+#  rotate - create new log file for each day, delete old files,
+#           see option <RotateLog>.
+WriteLog=append
 
-# Delete log file upon server start (only in server-mode) (yes, no).
-ResetLog=no
+# Log file rotation period (days).
+#
+# Defines how long to keep old log-files, when log rotation is active
+# (option <WriteLog> is set to "rotate").
+RotateLog=3
 
 # How error messages must be printed (screen, log, both, none).
 ErrorTarget=both

commit 14c654748ca2b0da938fdbfdb6917686a0847eec

fixed: when rotating log-files option TimeCorrection were not respected when bulding new file name - the filename could have wrong date stamp in the name (bug introduced in r1059)
git-svn-id: svn://svn.code.sf.net/p/nzbget/code/trunk@1080 b1b436ca-2b18-4f33-ab5d-510789358d54

daemon/util/Log.cpp
@@ -407,7 +407,7 @@ void Log::RotateLog()
 	snprintf(szFileMask, 1024, "%s-####-##-##%s", szBaseName, szBaseExt);
 	szFileMask[1024-1] = '\0';
 
-	time_t tCurTime = time(NULL);
+	time_t tCurTime = time(NULL) + g_pOptions->GetTimeCorrection();
 	int iCurDay = (int)tCurTime / 86400;
 	char szFullFilename[1024];
 